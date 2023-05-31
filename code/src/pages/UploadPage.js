a816be82f34c44df113d754b0d0cfd8a65be6d3b
Yuanze <2292175116@qq.com>
2023-05-31 21:42:49 +0800

    bugfix: upload file

--- code/src/pages/UploadPage.js
+++ code/src/pages/UploadPage.js
@@ -1,92 +1,66 @@
-import React from 'react';
-import { Form, Input, Button, Upload, message } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Form, Input, Button, Upload, Card } from 'antd';
 
-const UploadFilePage = () => {
-    const onFinish = async (values) => {
-        const { file, desc, sample_type, data_type } = values;
+function FileUploadForm() {
+    const [form] = Form.useForm();
+    const [file, setFile] = useState(null);
 
-        const formData = new FormData();
-        formData.append('file', file[0]);
-        formData.append('desc', desc);
-        formData.append('sample_type', sample_type);
-        formData.append('data_type', data_type);
-
-        try {
-            const response = await fetch('http://localhost:8080/api/dataset', {
-                method: 'POST',
-                body: formData,
-            });
-
-            const data = await response.json();
-            if (response.ok) {
-                // 请求成功，处理返回的数据
-                console.log(data);
-            } else {
-                // 请求失败，处理错误信息
-                message.error(data.error_msg);
-            }
-        } catch (error) {
-            // 发生错误，处理异常
-            console.error(error);
-            message.error('An error occurred. Please try again.');
-        }
+    const handleFileChange = (info) => {
+        setFile(info.file.originFileObj);
     };
 
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
+    const handleSubmit = () => {
+        form.validateFields().then((values) => {
+            const formData = new FormData();
+            formData.append('file', file);
+            formData.append('tag_type', values.tagType);
+            formData.append('sample_type', values.sampleType);
+            formData.append('desc', values.desc);
+
+            fetch('http://localhost:8080/api/dataset', {
+                method: 'POST',
+                body: formData,
+                credentials: 'include',
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    // 处理响应数据
+                    console.log(data);
+                })
+                .catch((error) => {
+                    // 处理错误
+                    console.error(error);
+                });
+        });
     };
 
     return (
-        <div style={{ maxWidth: '500px', margin: '0 auto' }}>
-            <h2>上传文件</h2>
-            <Form
-                name="uploadForm"
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                labelCol={{ span: 6 }}
-                wrapperCol={{ span: 18 }}
-            >
-                <Form.Item
-                    label="文件"
-                    name="file"
-                    rules={[{ required: true, message: '请选择要上传的文件' }]}
-                >
-                    <Upload beforeUpload={() => false}>
-                        <Button icon={<UploadOutlined />} type="primary">选择文件</Button>
-                    </Upload>
-                </Form.Item>
-
-                <Form.Item
-                    label="描述"
-                    name="desc"
-                    rules={[{ required: true, message: '请输入文件描述' }]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="样本类型"
-                    name="sample_type"
-                    rules={[{ required: true, message: '请输入样本类型' }]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="标记类型"
-                    name="data_type"
-                    rules={[{ required: true, message: '请输入标记类型' }]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item wrapperCol={{ offset: 6, span: 18 }}>
-                    <Button type="primary" htmlType="submit">上传</Button>
-                </Form.Item>
-            </Form>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Card style={{ width: 400 }}>
+                <Form form={form} onFinish={handleSubmit}>
+                    <Form.Item label="文件" name="file">
+                        <Upload onChange={handleFileChange}>
+                            <Button>Select File</Button>
+                        </Upload>
+                    </Form.Item>
+                    <Form.Item label="标签类型" name="tagType">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="样本类型" name="sampleType">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="描述" name="desc">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            上传
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
         </div>
     );
-};
+}
 
-export default UploadFilePage;
+export default FileUploadForm;
